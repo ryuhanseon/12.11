4946c27045a6c4051d605500b0b831e59ef1ae57
깃hanseon97@naver.com <hanseon97@naver.com>
2023-12-11 18:00:11 +0900

    주석처리한 풀이

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,63 +1,51 @@
 package org.example;
 
+import lombok.Builder;
+import lombok.experimental.Delegate;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
-public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+public class Main { //Main 클래스를 지정
+    public static void main(String[] args) { //main 메서드를 지정
+        Scanner sc = new Scanner(System.in); // 각 출력을 위한 Scanner를 실행
+        List<WiseSaying> wiseSayingList = new ArrayList<>(); //ArrayList를 이용해서 wiseSaying객체를 만들고 후에 이용한다.
+        long id = 1;  // long 타입의 변수 id의 값을 1로 초기화
 
-        System.out.println("==명언 앱==");
-        int id = 1;
-        List<WiseSaying> sayingList = new ArrayList<>();
-        while (true) {
-            System.out.println("명령) ");
-            String command = sc.nextLine();
+        System.out.println("== 명언앱 ==");  //"==명언웹==" 이라는 것을 출력
 
-            if (command.equals("종료")) {
-                break;
-            } else if (command.equals("등록")) {
-                System.out.println("명언: ");
-                String content = sc.nextLine();
-                System.out.println("작가: ");
-                String author = sc.nextLine();
+        while (true) {    //반복문 안에 true값을 이용해서 무한루프를 돌린다.
+            System.out.printf("명령) ");  //명령을 출력한다.
+            String command = sc.nextLine(); //문자열 String를 command라 정하고 스캐너의 sc를 이용해서 바로위의 명령에 값을 넣을 수 있게끔한다.
 
-                WiseSaying wiseSaying = new WiseSaying(content, author);
-                sayingList.add(wiseSaying);
+            if (command.equals("종료")) { //반복문안에서 if문을 사용해서 먼저 종료를 만들어준다.
+                break;  //종료를 입력하면 탈출할 수 있게 해준다.
+            } else if (command.equals("등록")) { // 종료 이외에 command의 값이 "등록"이라 입력하면 명언으로 이동한다.
+                System.out.printf("명언: "); // 명언: 을 출력한다.
+                String content = sc.nextLine(); //content를 지정해주고 바로 위 명언에 입력을 받을 수 있게끔 한다.
+                System.out.printf("작가: "); // 작가: 를 출력한다.
+                String author = sc.nextLine(); //author를 지정해주고 바로 위 작가에 값을 입력해줄수 있게끔 한다.
+                System.out.println(id + "번 명언이 등록 되었습니다."); // 반복문 밖에 있는 id의 값과 나머지 "번 명언이 등록 되었습니다.를 출력해준다.
+
+                WiseSaying wiseSaying = new WiseSaying(id, author, content); // wiseSaying의 객체를 지정해주고 그 안에다 각 id, author, content의 값을 넣어준다.
+                wiseSayingList.add(wiseSaying); // wiseSayingList의 값에 wiseSaying의 값을 넣어준다.
+
+                id++; // id의 값이 늘어날 수 있게끔 ++를 붙여준다.
+
+            } else if (command.equals("목록")) { // 종료나 등록 이외가 목록일 시
+                System.out.println("번호 / 작가 / 명언"); // 번호 작가 명언을 출력한다.
+                for (WiseSaying wiseSaying : wiseSayingList) { //목록의 들어갈 값들을 선별하기 위해서 for each문을 시행한다.
+                    System.out.printf("%d, %s, %s \n", wiseSaying.getId(), wiseSaying.getAuthor(), wiseSaying.getContent());
+                }  //각 정수들이 들어갈 것을 염두하고 %d등을 넣어주고 wiseSaying 객체에 각 생성자에서 지정한 값을 가져오고
             }
-            //목록
-            else if (command.equals("목록")) {
+        }
 
-                System.out.println("번호/ 작가/ 명언 ");
-                System.out.println("---------------");
-                for (WiseSaying saying : sayingList) {
-                    System.out.println(id + "/" + saying.getAuthor() + "/" + saying.getContent());
-                    id++;
-                }
-            }
 
-        }   sc.close();
+        sc.close(); //반복문이 더이상 시행되지 않도록 닫아준다.
     }
 }
 
-class WiseSaying {
-    private String content;
-    private String author;
 
-    WiseSaying(String content, String author) {
-        this.content = content;
-        this.author = author;
-    }
-
-    String getContent() {
-        return content;
-    }
-
-    String getAuthor() {
-        return author;
-    }
-
-}
--- src/main/java/org/example/WiseSaying.java
+++ src/main/java/org/example/WiseSaying.java
@@ -1,2 +1,26 @@
-package org.example;public class WiseSaying {
+package org.example;
+
+class WiseSaying {
+    private long id;
+    private String author;
+    private String content;
+
+    WiseSaying (long id, String author, String content) {
+        this.id = id;
+        this.author = author;
+        this.content = content;
+    }
+
+
+    public long getId() {
+        return this.id;
+    }
+
+    public String getAuthor() {
+        return this.author;
+    }
+
+    public String getContent() {
+        return this.content;
+    }
 }
